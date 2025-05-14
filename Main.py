9c159b046feb250a0b3e74d635f6ca661b5a43ac
sushith <sushithks@gmail.com>
2025-05-14 17:53:16 -0400

    Second function - user data udf df

--- Main.py
+++ Main.py
@@ -56,7 +56,7 @@ def process_log_data(spark, input_data,output_data):
 
     # create timestamp column from original timestamp column
     get_timestamp = udf(lambda x : datetime.utcfromtimestamp(int(x)/1000), TimestampType())
-
+    df = df.withColumn("start_time", get_timestamp("ts"))
 
     time_table = df.withColumn("hour",hour("start_time"))\
                     .withColumn("day",dayofmonth("start_time"))\
