b3636b58622126524e96131e857aa6ed428434e8
sushith <sushithks@gmail.com>
2025-05-15 18:57:37 -0400

    Orchestration- Initialization

--- Dag/dag.py
+++ Dag/dag.py
@@ -11,3 +11,11 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5)
 }
+
+
+dag = DAG(
+    'songs_de',
+    default_args=default_args,
+    description='Songs Data analysis',
+    schedule_interval=timedelta(days=1)
+)
